ed6d70dfb78a57c9c397a70bb984d6727b2bc347
Alex Fedoseev <lxfedoseev@gmail.com>
2019-01-07 13:22:58 +0300

    Consistency view controller added

---
+++ Pasta/Controllers/ConsistencyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConsistencyViewController.swift
+//  Pasta
+//
+//  Created by Alex Fedoseev on 07.01.2019.
+//  Copyright © 2019 Alex Fedoseev. All rights reserved.
+//
+
+import UIKit
+
+class ConsistencyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
