a8efd787ab99d34cc630891b8a0fb4695832aa73
Alex Fedoseev <lxfedoseev@gmail.com>
2019-10-14 20:48:21 +0300

    game branch

---
+++ Pasta/Controllers/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  Pasta
+//
+//  Created by Alex Fedoseev on 14.10.2019.
+//  Copyright © 2019 Alex Fedoseev. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
